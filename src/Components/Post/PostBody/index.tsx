7a4913274bd16acfdb6d3ae45d6a35cbe138e736
João Paulo Oliveira <jpaulo2212gt@gmail.com>
2019-08-25 21:36:24 -0300

    building the markdown structure

--- src/Components/Post/PostBody/index.tsx
+++ src/Components/Post/PostBody/index.tsx
@@ -1,17 +1,20 @@
 // Import Declaration
 import * as React from 'react';
+import marked from 'marked';
 
 interface Props {
   body: string;
 }
 
 export function PostBody({ body }: Props) {
+  const md = marked(body);
+  console.log(md);
   return (
     <div className="row">
       <div className="col-md-12">
-        <p className="text-justify nunito" data-cy="post-body">
-          {body}
-        </p>
+        <div className="text-justify nunito" data-cy="post-body">
+          {md}
+        </div>
       </div>
     </div>
   );
